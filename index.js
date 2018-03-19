5f57ef27de606d8566c38b1e45a005d105114407
Sandy Edwards <peterjamesedwards@gmail.com>
2018-03-19 17:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(kittenName) {
+  kittens.push(kittenName);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kittenName) {
+  kittens.unshift(kittenName);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(kittenName) {
+  kittens = [...kittens, kittenName];
+  return kittens;
+}
